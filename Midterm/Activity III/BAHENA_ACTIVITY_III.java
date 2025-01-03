7efd030a9af34343b8dbdc4c7413b058f59a02c4
Kirk Bahena <bahenakirk@gmail.com>
2025-01-03 13:55:16 +0800

    Add files via upload

---
+++ Midterm/Activity III/BAHENA_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+public class BAHENA_III {
+    public static void main (String [] args){
+       int x = 5;
+        while (x >= 0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
